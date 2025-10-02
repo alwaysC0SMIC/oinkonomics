e03804bf830a60eee34c09fa6e018c418ef4e496
alwaysC0SMIC <stuartproctor@outlook.com>
2025-10-02 13:43:38 +0200

    Project Setup

---
+++ build.gradle.kts
@@ -0,0 +1,7 @@
+plugins {
+    id("com.android.application") version "8.13.0" apply false
+    id("com.android.library")    version "8.13.0" apply false // if you have library modules
+    kotlin("android")            version "2.0.20" apply false
+}
+
+
